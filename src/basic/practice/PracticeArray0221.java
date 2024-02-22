59b7bac2209dbd2f994970c9bb8899ad85974586
gayeon <ini03134@naver.com>
2024-02-22 18:12:36 +0900

    # Java 기초 문법(메서드)

--- src/basic/practice/PracticeArray0221.java
+++ src/basic/practice/PracticeArray0221.java
@@ -19,39 +19,7 @@ public class PracticeArray0221 {
         // 6. 찾은 인덱스를 통해 새로운 이름으로 수정한다.
         // 7. 위 내용을 수정이 정확히 완료될때까지 반복한다.
 
-        System.out.println("수정할 친구를 입력해주세요");
-        System.out.println("수정을 중단하고 싶으면 '중단' 이라고 입력해주세요");
-        System.out.print("> ");
-        String name = sc.next();
 
-        boolean flag = false;
-        for (int i = 0; i < kakao.length ; i++) {
-            if(name.equals(kakao[i])) {
-                System.out.println("탐색 성공! ");
-                System.out.println("인덱스 번호: " + i );
-                flag = true;
-
-                System.out.println("수정할 친구의 이름을 입력해주세요");
-                System.out.print("> ");
-                String newName = sc.next();
-                kakao[i] = newName;
-
-            }
-            if(name.equals("중단")){
-                System.out.println("수정이 중단 되었습니다.");
-                break;
-            }
-
-        }
-
-        if(!flag){
-            System.out.println(name + "이라는 친구는 없습니다.");
-        }
-
-        System.out.println("수정된 친구들: " + Arrays.toString(kakao));
-
-
-        sc.close();
 
     }
 }
