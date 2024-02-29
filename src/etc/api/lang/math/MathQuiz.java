99b831869c88099d01842fc84e933ed02b99cd88
gayeon <ini03134@naver.com>
2024-02-29 18:21:49 +0900

    # Java api (lang)

---
+++ src/etc/api/lang/math/MathQuiz.java
@@ -0,0 +1,29 @@
+package etc.api.lang.math;
+
+import java.util.Stack;
+
+public class MathQuiz {
+
+
+    static int page(int n) {
+
+        return (int) Math.ceil(n /10.0);
+
+    }
+    public static void main(String[] args) {
+
+    /*
+     1 ~ 10이 전달되면 1이 반환되고,
+     11 ~ 20이 전달되면 2가 반환되고,
+     21 ~ 30이 전달되면 3이 반환되고.......
+     이러한 로직을 수행하는 static 메서드 page를 선언해 보세요.
+     */
+
+        System.out.println(page(80));
+
+
+
+    }
+
+
+}
